e9e26afa6636f92a991021a91d8b3b5d1972151a
johnkauan <johnkauanpassos123@gmail.com>
2026-02-26 15:49:58 -0300

    criando model

--- src/main/java/dev/john/CadastroDeComida/model/FoodModel.java
+++ src/main/java/dev/john/CadastroDeComida/model/FoodModel.java
@@ -1,4 +1,24 @@
 package dev.john.CadastroDeComida.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_food")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class FoodModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private Integer quantidade;
+    private LocalDate dataValidade;
+
 }
